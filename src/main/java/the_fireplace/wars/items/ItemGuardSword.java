5f88024d7cd1c8bc4d2fd339ee91de9c4f2362c8
The_Fireplace <the.f1repl4ce@gmail.com>
2016-08-05 20:03:40 -0500

    Work

--- src/main/java/the_fireplace/wars/items/ItemGuardSword.java
+++ src/main/java/the_fireplace/wars/items/ItemGuardSword.java
@@ -1,5 +1,6 @@
 package the_fireplace.wars.items;
 
+import com.google.common.collect.Multimap;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
@@ -11,12 +12,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import the_fireplace.wars.WarsMod;
 import the_fireplace.wars.init.WarsBlocks;
-
-import com.google.common.collect.Multimap;
 import the_fireplace.wars.init.WarsItems;
 
 public class ItemGuardSword extends Item {
@@ -30,18 +27,18 @@ public class ItemGuardSword extends Item {
 		this.setMaxDamage(300);
 		this.weaponDamage = 3F;
 		setFull3D();
-
 	}
 
+	@Override
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
 		if (player instanceof EntityPlayerMP && !ItemArmorMod.fullEquiped(player, WarsItems.guardArmor)) {
 			player.addChatMessage(new ChatComponentText("\u00a74DONT CHEAT! \u00a72Wear The Guard Armour (which you must put on in survival)"));
 			stack.stackSize = 0;
-
 		}
 		return false;
 	}
 
+	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
 		player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
 		return itemStack;
@@ -51,30 +48,28 @@ public class ItemGuardSword extends Item {
 	 * Returns the strength of the stack against a given block. 1.0F base,
 	 * (Quality+1)*2 if correct blocktype, 1.5F if sword
 	 */
+	@Override
 	public float getStrVsBlock(ItemStack par1ItemStack, Block par2Block) {
 		return par2Block != WarsBlocks.sumBlock ? 0.9F : 15F;
 	}
 
+	@Override
 	public EnumAction getItemUseAction(ItemStack par1ItemStack) {
 		return EnumAction.BLOCK;
 	}
 
-	@SideOnly(Side.CLIENT)
-	// Makes it render nicely
-	public boolean isFull3D() {
-		return true;
-	}
-
 	// The max use time of the action
+	@Override
 	public int getMaxItemUseDuration(ItemStack par1ItemStack) {
 		return 72000;
 	}
 
 	
     @SuppressWarnings({ "rawtypes", "unchecked" })
-	public Multimap getItemAttributeModifiers()
+	@Override
+	public Multimap getAttributeModifiers(ItemStack stack)
     {
-        Multimap multimap = super.getItemAttributeModifiers();
+        Multimap multimap = super.getAttributeModifiers(stack);
         multimap.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(), new AttributeModifier(itemModifierUUID, "Weapon modifier", (double)this.weaponDamage, 0));
         return multimap;
     }
